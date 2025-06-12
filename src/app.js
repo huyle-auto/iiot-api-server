410ebc68bcd739e85e1da5cf4de9cf148345a1e9
huyle-auto <lequanghuyqn123@gmail.com>
2025-06-12 10:47:46 +0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ const corsOptions = {
     credentials: true
 }
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+// app.options('*', cors(corsOptions));
 
 app.use(cookieParser());
 
