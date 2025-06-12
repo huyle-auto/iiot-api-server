10631687add6d681345de82aebcbf811dd7b3c61
huyle-auto <lequanghuyqn123@gmail.com>
2025-06-12 13:37:14 +0700

    Update app.js
    Remove server's cors to use azure's cors

--- src/app.js
+++ src/app.js
@@ -8,28 +8,28 @@ import { errorHandler } from './middlewares/error.middleware.js';
 const app = express();
 
 // Middlewares
-const allowedOrigin = 'https://salmon-field-08f48e900.6.azurestaticapps.net';
-const corsOptions = {
-    origin: allowedOrigin,
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-};
+// const allowedOrigin = 'https://salmon-field-08f48e900.6.azurestaticapps.net';
+// const corsOptions = {
+//     origin: allowedOrigin,
+//     credentials: true,
+//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//     allowedHeaders: ['Content-Type', 'Authorization']
+// };
 
-// Apply to all routes
-app.use(cors(corsOptions));
+// // Apply to all routes
+// app.use(cors(corsOptions));
 
-// Handle preflight explicitly (optional but safest)
-app.use((req, res, next) => {
-  if (req.method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Origin', allowedOrigin);
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    return res.sendStatus(204);
-  }
-  next();
-});
+// // Handle preflight explicitly (optional but safest)
+// app.use((req, res, next) => {
+//   if (req.method === 'OPTIONS') {
+//     res.header('Access-Control-Allow-Origin', allowedOrigin);
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//     res.header('Access-Control-Allow-Credentials', 'true');
+//     return res.sendStatus(204);
+//   }
+//   next();
+// });
 
 app.use(express.json());
 app.use(cookieParser());
